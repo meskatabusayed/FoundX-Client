0e42e724094db30c533332b4bc54cc80d033cd0d
meskatabusayed <meskatabusayed@gmail.com>
2024-09-14 18:06:50 +0530

    initialized

---
+++ src/app/found-items/page.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+import React from "react";
+
+const page = () => {
+  return <div>
+    This is Fond items
+  </div>;
+};
+
+export default page;
