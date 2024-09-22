30750bf861e8d62560c240034efd53bf91a57292
meskatabusayed <meskatabusayed@gmail.com>
2024-09-23 00:27:19 +0530

    middleware added

--- src/services/RecentPost/index.ts
+++ src/services/RecentPost/index.ts
@@ -1,14 +1,12 @@
 import envConfig from "@/src/config/envConfig";
-import { delay } from "@/src/utils/dely";
 
 
 const getRecentPost = async () => {
-
-  const res = await fetch(`${envConfig.baseApi}/items?sortBy=-createdAt&limit=9`);
-  
-  await delay(5000);
+  const res = await fetch(
+    `${envConfig.baseApi}/items?sortBy=-createdAt&limit=9`
+  );
 
   return res.json();
-}
+};
 
 export default getRecentPost;
