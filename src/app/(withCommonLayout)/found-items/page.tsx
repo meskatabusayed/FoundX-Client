7f1fec9f1986ae59cbcc25542755ed5b30c41262
meskatabusayed <meskatabusayed@gmail.com>
2024-09-27 23:54:59 +0530

    items found

--- src/app/(withCommonLayout)/found-items/page.tsx
+++ src/app/(withCommonLayout)/found-items/page.tsx
@@ -1,12 +1,18 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable react/self-closing-comp */
-/* eslint-disable prettier/prettier */
-import React from "react";
+// import Container from "@/src/components/UI/Container";
+// import Post from "@/src/components/UI/Post";
+import Container from "@/src/components/ui/Container";
+import { AxiosInstance } from "@/src/lib/AxiosInstance";
+// import axiosInstance from "@/src/lib/AxiosInstance";
+import { IPost } from "@/src/types";
 
-const page = () => {
-  return <div>
-    This is Fond items
-  </div>;
-};
+export default async function FoundItems() {
+  const { data } = await AxiosInstance.get(`/items`);
 
-export default page;
+  return (
+    <Container>
+      <div className="mx-auto my-3 max-w-[720px]">
+        {/* {data?.data?.map((post: IPost) => <Post key={post?._id} post={post} />)} */}
+      </div>
+    </Container>
+  );
+}
