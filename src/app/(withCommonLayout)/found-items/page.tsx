7a31b60c004d542ab422ec9ae9bc68a3d61a95a9
meskatabusayed <meskatabusayed@gmail.com>
2024-09-28 02:19:05 +0530

    found-items page added

--- src/app/(withCommonLayout)/found-items/page.tsx
+++ src/app/(withCommonLayout)/found-items/page.tsx
@@ -1,8 +1,7 @@
-// import Container from "@/src/components/UI/Container";
-// import Post from "@/src/components/UI/Post";
 import Container from "@/src/components/ui/Container";
+import Post from "@/src/components/ui/Post";
 import { AxiosInstance } from "@/src/lib/AxiosInstance";
-// import axiosInstance from "@/src/lib/AxiosInstance";
+
 import { IPost } from "@/src/types";
 
 export default async function FoundItems() {
@@ -11,7 +10,7 @@ export default async function FoundItems() {
   return (
     <Container>
       <div className="mx-auto my-3 max-w-[720px]">
-        {/* {data?.data?.map((post: IPost) => <Post key={post?._id} post={post} />)} */}
+        {data?.data?.map((post: IPost) => <Post key={post?._id} post={post} />)}
       </div>
     </Container>
   );
